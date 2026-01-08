320e55af13cc76831f8a1c9f6a26d323744e5811
Gian Carl <iamgiancarlpo@gmail.com>
2026-01-08 21:28:57 +0800

    modified vite

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/sass/app.scss',
                 'resources/js/homepage.js',
                 'resources/js/invitation.js',
+                'resources/js/admin-rsvp.js',
             ],
             refresh: true,
         }),
